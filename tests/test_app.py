b9f77cec43ff684277a55ee2bc2736fba1e34c5e
Saima Abdus <saima.abdus@kraken.tech>
2025-07-11 15:56:08 +0100

    Break test to test CI

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,3 +2,7 @@ def test_get_home(web_client):
     response = web_client.get("/")
     assert response.status_code == 200
     assert response.data.decode("utf-8") == "Hello, world!"
+
+
+def test_failing_example():
+    assert 1 == 2
